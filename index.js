6b36a3d734a5ab4591e0a57698425b9d410a329b
harrydeluxe <hanek@harrydeluxe.de>
2012-03-15 18:40:23 +0100

    add package for npm

---
+++ index.js
@@ -0,0 +1,13 @@
+(function () {
+	'use strict';
+
+	module.exports = {
+		'array': require('./array.js'),
+		'base64': require('./base64.js'),
+		'char': require('./char.js'),
+		'md5': require('./md5.js'),
+		'string': require('./string.js'),
+		'utf8': require('./utf8.js')	
+	};
+	
+}());
